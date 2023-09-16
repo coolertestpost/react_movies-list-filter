871c1b9479d03709ca26286f9bf1621042400271
coolertestpost <sl1754sl@gmail.com>
2023-09-16 14:31:41 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  { /* eslint-disable-next-line max-len */ }
+
+  const [visibleMovies, setVisibleMovies] = useState<Movie[]>([...moviesFromServer]);
+
+  const filterMovies = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+
+    if (!event.target.value) {
+      setVisibleMovies([...moviesFromServer]);
+
+      return;
+    }
+
+    setVisibleMovies([...moviesFromServer].filter((movie) => {
+      const movieDescription = movie.description.toLowerCase();
+      const movieTitle = movie.title.toLowerCase();
+
+      const q = event.target.value.toLowerCase().trim();
+
+      if (!q) {
+        return;
+      }
+
+      { /* eslint-disable-next-line max-len */ }
+
+      return movieDescription.match(new RegExp(q, 'g'))?.length || movieTitle.match(new RegExp(q, 'g'))?.length;
+    }));
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +53,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={filterMovies}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">
